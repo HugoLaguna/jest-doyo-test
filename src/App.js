665a6257d67071cbbd28e5bb138389f9adfbfef2
HugoLaguna <hugo@experimentality.co>
2021-05-18 13:54:09 -0500

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ title }) {
+  const [ data, setData] = useState();
+
+  useEffect(() => {
+    const getData = async ()=>{
+      const response = await fetch("http://localhost:3000/api/catalog_system/pub/products/search/ropa")
+      const dataRAW = await response.json()
+      setData(dataRAW)
+    }
+    getData()
+  }, [])
+ 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> {title} </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +31,12 @@ function App() {
         >
           Learn React
         </a>
+        <p> 
+          Busqueda: {
+            data &&  data.length 
+            } productos
+        </p>
+
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen , act} from '@testing-library/react';
 import App from './App';
+import mockFetch from './__mock__/search';
+
+const props = {
+  title: "EXPERIMENTALITY"
+}
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App {...props}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/EXPERIMENTALITY/i);
+  expect(titleElement).toBeInTheDocument();
 });
+
+
+describe("Services Mocks", () => {
+
+  // mock FETCH
+  let originalFetch;
+  beforeEach(() => {
+      originalFetch = global.fetch;
+      global.fetch = jest.fn(() => Promise.resolve({
+          json: () => Promise.resolve(mockFetch)
+      }));
+  });
+  afterEach(() => {
+      global.fetch = originalFetch;
+  });
+
+  it("API Service", async () =>{
+    await act(async ()=> render(<App  {...props}/>))
+    expect(screen.getByText(/\d/i)).toBeInTheDocument()
+  })
+
+})
+
